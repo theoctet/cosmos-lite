5c346cdfd681d6643e77c17f9f4db6027b8a44fc
Srinivas Baride <b.srinivas8687@gmail.com>
2020-01-18 19:13:24 +0530

    Added mint module queries

---
+++ mint.go
@@ -0,0 +1,23 @@
+package cosmoslite
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/mint"
+)
+
+func (c *CosmosLite) QueryParameters() (result mint.Params, err error) {
+	err = c.query(fmt.Sprintf("custom/%s/%s", mint.QuerierRoute, mint.QueryParameters), nil, &result)
+	return result, err
+}
+
+func (c *CosmosLite) QueryInflation() (result sdk.Dec, err error) {
+	err = c.query(fmt.Sprintf("custom/%s/%s", mint.QuerierRoute, mint.QueryInflation), nil, &result)
+	return result, err
+}
+
+func (c *CosmosLite) QueryAnnualProvisions() (result sdk.Dec, err error) {
+	err = c.query(fmt.Sprintf("custom/%s/%s", mint.QuerierRoute, mint.QueryAnnualProvisions), nil, &result)
+	return result, err
+}
